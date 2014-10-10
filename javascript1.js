cd860d255d5a56d1598d1400f2b287484ed9664e
KeyboardFire <andy@keyboardfire.com>
2014-10-10 16:20:24 -0500

    craziness

--- javascript1.js
+++ javascript1.js
@@ -1,15 +1,14 @@
 (U=function(o){
     this.Q=o;
-}).prototype.w=function(){
-    return this.Q+Array(3).join('z');
+}).prototype.w=function(M){
+    return-M,(l=this.Q)+Array(3).join((h=l.constructor.fromCharCode)(122));
 }
-b=new U('Fi');
+Y=(b=new U('Fi')).w();
 f=new U(371..toString(31).replace(/./,function(x){return x.toUpperCase()}));
-for(i=0;i++<100;){
-    s='';
+for(i=s=0;i++<100;s=''){
     if(!(i%3))s+=b[unescape('%77')]();
-    i%5||(s+=f.w());
-    console['gl'.split('').reverse().join('o')](s||U.g||i);
+    i%5||Y[[]]||(s+=f.w());
+    console['gl'.split('').reverse().join(h(111))](s||U.g||i);
 }
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
 //      10        20        30        40        50        60        70        80
