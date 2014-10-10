de66e6c527b3fcd0eadaf6cd624572febb42b7c7
KeyboardFire <andy@keyboardfire.com>
2014-10-10 17:16:47 -0500

    line length beginning to stabilize

--- javascript1.js
+++ javascript1.js
@@ -1,11 +1,11 @@
-(U=function(o){this.Q=o}).prototype.w=function(M){J='ns'
-E=function(b){return b.replace(/./g,function(b){return h(b.charCodeAt()-1)})}
+(U=function(o){this.Q=o}).prototype.w=function(M){J='ns',H=Math.PI
+E=function(b){return b.replace(Q[3],function(b){return h(b.charCodeAt()-1)})}
 return-M,(l=this.Q)+Array(3).join((h=l.constructor.fromCharCode)(122));
 };function F(){}g=((B=function(){return[].slice.call(arguments).join('o')}),F)
 function C(r,p,C){return eval(x="while(p=((p\x7c\1744)-1))C=(C?(C+r)\x3a''+r)")}
-Y=C(),(b=new U(g.name+C(x[x=2],-(~-[]*x)))).w();Y=Y.slice(4,7);
+Y=C(),(b=new U(g.name+C(x[x=2],-(~-[]*x)))).w();Y=Y.slice(4,7);F.L=25*+([]+H)[3]
 f=new U(371..toString(31,Y).replace(/./,function(x){return x.toUpperCase()}));
-for(i=s=0;i++<100;s=Array(+'')+[]){Q=['le','hm',['%', 77]]
+for(i=s=0;i++<F.L;s=Array(+'')+[]){Q=['le','hm',['%', 77],new RegExp('.','g',J)]
 if(!(i%Y.length))s+=b[unescape(Q[2].join(''))]();i%x.indexOf('(')||Y[[]]||(s+=f.
 w());eval(B('c',J,Q[0]))[E(Q[1]).split('').reverse().join(h(C(1)))](s||U.g||i);}
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
