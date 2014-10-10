76e97de6b6a8214038b2a39be5d39653a9452f17
KeyboardFire <andy@keyboardfire.com>
2014-10-10 16:13:41 -0500

    "cleanup" (ha)

--- javascript1.js
+++ javascript1.js
@@ -1,13 +1,15 @@
-(U = function(o) {
+(U=function(o){
     this.Q=o;
-}).prototype.w = function() {
+}).prototype.w=function(){
     return this.Q+Array(3).join('z');
 }
-
-b=new U('Fi'),f=new U('Bu');
-for (i=0;i++<100;) {
+b=new U('Fi');
+f=new U(371..toString(31).replace(/./,function(x){return x.toUpperCase()}));
+for(i=0;i++<100;){
     s='';
-    if(!(i%3))s+=b.w();
+    if(!(i%3))s+=b[unescape('%77')]();
     i%5||(s+=f.w());
-    console['log'](s||U.g||i);
+    console['gl'.split('').reverse().join('o')](s||U.g||i);
 }
+//345678901234567890123456789012345678901234567890123456789012345678901234567890
+//      10        20        30        40        50        60        70        80
