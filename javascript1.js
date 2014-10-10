b36398b6291747860fd70e989e1e2743249d57e1
KeyboardFire <andy@keyboardfire.com>
2014-10-10 16:02:06 -0500

    ...no comment

--- javascript1.js
+++ javascript1.js
@@ -1,14 +1,13 @@
-function U(o) {
-    this.Q = o;
-}
-U.prototype.w = function() {
-    return this.Q + Array(3).join('z');
+(U = function(o) {
+    this.Q=o;
+}).prototype.w = function() {
+    return this.Q+Array(3).join('z');
 }
 
-var b = new U('Fi'), f = new U('Bu');
-for (var i = 0; i++ < 100;) {
-    var s = '';
-    if (i % 3 == 0) s += b.w();
-    if (i % 5 == 0) s += f.w();
-    console['log'](s || i);
+b=new U('Fi'),f=new U('Bu');
+for (i=0;i++<100;) {
+    s='';
+    if(!(i%3))s+=b.w();
+    i%5||(s+=f.w());
+    console['log'](s||U.g||i);
 }
