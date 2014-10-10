a6c061c0f9c08927731ba9f25bd2769abd20dd22
KeyboardFire <andy@keyboardfire.com>
2014-10-10 15:58:28 -0500

    anti-refactoring

--- javascript1.js
+++ javascript1.js
@@ -1,14 +1,14 @@
-function FBManager(prefix) {
-    this.prefix = prefix;
+function U(o) {
+    this.Q = o;
 }
-FBManager.prototype.getFB = function() {
-    return this.prefix + Array(3).join('z');
+U.prototype.w = function() {
+    return this.Q + Array(3).join('z');
 }
 
-var fizz = new FBManager('Fi'), buzz = new FBManager('Bu');
+var b = new U('Fi'), f = new U('Bu');
 for (var i = 0; i++ < 100;) {
     var s = '';
-    if (i % 3 == 0) s += fizz.getFB();
-    if (i % 5 == 0) s += buzz.getFB();
-    console.log(s || i);
+    if (i % 3 == 0) s += b.w();
+    if (i % 5 == 0) s += f.w();
+    console['log'](s || i);
 }
