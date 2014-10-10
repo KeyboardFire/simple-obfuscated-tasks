11183dddef9b451e68f2fba663031a31ba68a22c
KeyboardFire <andy@keyboardfire.com>
2014-10-10 17:31:08 -0500

    hooray! lines line up!

--- javascript1.js
+++ javascript1.js
@@ -1,11 +1,11 @@
-(U=function(o){this.Q=o}).prototype.w=function(M){J='ns',H=Math.PI,G='.',T=3
-E=function(b){return b.replace(Q[T],function(b){return h(b.charCodeAt()-H+T)})}
+(U=function(o){this.Q=o}).prototype.w=function(M){J='ns',H=Math.PI,G='.',T=H|0+G
+E=function(b){return b.replace(Q[T],function(b){return h(b.charCodeAt()-H+T)})};
 return-M,(l=this.Q)+Array(T).join((h=l.constructor.fromCharCode)(0x79+U.length))
 };function F(){}g=((B=function(){return[].slice.call(arguments).join('o')}),F)//
 function C(r,p,C){return eval(x="while(p=((p\x7c\1744)-1))C=(C?(C+r)\x3a''+r)")}
 Y=C(),(b=new U(g.name+C(x[x=2],-(~-[]*x)))).w();Y=Y.slice(4,7);F.L=25*+([]+H)[T]
 f=new U(371..toString(31,Y).replace(/./,function(x){return x.toUpperCase()}));{}
-for(i=s=0;i++<F.L;s=Array(+'')+[]){Q=['le','hm',['%',T+74],new RegExp(G,'g',J)]
+for(i=s=0;i++<F.L;s=Array(+'')+[]){Q=['le','hm',['%',T+74],new RegExp(G,'g',J)];
 if(!(i%Y.length))s+=b[unescape(Q[2].join(''))]();i%x.indexOf('(')||Y[[]]||(s+=f.
 w());eval(B('c',J,Q[0]))[E(Q[1]).split('').reverse().join(h(C(1)))](s||U.g||i);}
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
