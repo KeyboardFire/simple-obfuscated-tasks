b3b2ab7d72a8d9db54c9f36bca167d26674627e9
KeyboardFire <andy@keyboardfire.com>
2014-10-10 16:28:25 -0500

    yay more functions

--- javascript1.js
+++ javascript1.js
@@ -3,12 +3,15 @@
 }).prototype.w=function(M){
     return-M,(l=this.Q)+Array(3).join((h=l.constructor.fromCharCode)(122));
 }
+function C(r,p,C){
+    return eval("while(p=((p||4)-1))C=(C?(C+r):''+r)")
+}
 Y=(b=new U('Fi')).w();
 f=new U(371..toString(31).replace(/./,function(x){return x.toUpperCase()}));
 for(i=s=0;i++<100;s=''){
     if(!(i%3))s+=b[unescape('%77')]();
     i%5||Y[[]]||(s+=f.w());
-    console['gl'.split('').reverse().join(h(111))](s||U.g||i);
+    console['gl'.split('').reverse().join(h(C(1)))](s||U.g||i);
 }
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
 //      10        20        30        40        50        60        70        80
