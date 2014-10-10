ce3408ee8460ea0e5641a1352bba3029ad32cb11
KeyboardFire <andy@keyboardfire.com>
2014-10-10 07:13:41 -0500

    adding complexity...

--- javascript1.js
+++ javascript1.js
@@ -1,4 +1,12 @@
-for (var i = 0; i < 100; ++i) {
+function FBManager(prefix) {
+    this.prefix = prefix;
+}
+FBManager.prototype.getFB = function() {
+    return this.prefix + 'zz';
+}
+
+var fizz = new FBManager('Fi'), buzz = new FBManager('Bu');
+for (var i = 1; i <= 100; ++i) {
     var s = '';
     if (i % 3 == 0) s += 'Fizz';
     if (i % 5 == 0) s += 'Buzz';
