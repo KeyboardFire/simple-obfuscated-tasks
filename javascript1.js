c29436e4627bade1b0252321c0b3406c8b7ae7eb
KeyboardFire <andy@keyboardfire.com>
2014-10-10 17:03:28 -0500

    oh dear

--- javascript1.js
+++ javascript1.js
@@ -1,12 +1,12 @@
 (U=function(o){this.Q=o}).prototype.w=function(M){
 E=function(b){return b.replace(/./g,function(b){return h(b.charCodeAt()-1)})}
 return-M,(l=this.Q)+Array(3).join((h=l.constructor.fromCharCode)(122));
-};function F(){}g=F
+};function F(){}g=((B=function(){return[].slice.call(arguments).join('o')}),F)
 function C(r,p,C){return eval(x="while(p=((p\x7c\1744)-1))C=(C?(C+r)\x3a''+r)")}
 Y=C(),(b=new U(g.name+C(x[x=2],-(~-[]*x)))).w();Y=Y.slice(4,7);
 f=new U(371..toString(31,Y).replace(/./,function(x){return x.toUpperCase()}));
 for(i=s=0;i++<100;s=Array(+'')+[]){
 if(!(i%Y.length))s+=b[unescape('%77')]();i%x.indexOf('(')||Y[[]]||(s+=f.w())
-console[E('hm').split('').reverse().join(h(C(1)))](s||U.g||i);}
+eval(B('c','ns','le'))[E('hm').split('').reverse().join(h(C(1)))](s||U.g||i);}
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
 //      10        20        30        40        50        60        70        80
