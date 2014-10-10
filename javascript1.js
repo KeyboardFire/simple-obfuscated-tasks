04e18c1685a910496f55ac8c60e7ed510eee1a8d
KeyboardFire <andy@keyboardfire.com>
2014-10-10 16:46:55 -0500

    generic obfuscation

--- javascript1.js
+++ javascript1.js
@@ -1,16 +1,11 @@
-(U=function(o){
-    this.Q=o;
-}).prototype.w=function(M){
+(U=function(o){this.Q=o}).prototype.w=function(M){
     return-M,(l=this.Q)+Array(3).join((h=l.constructor.fromCharCode)(122));
-}
-function C(r,p,C){
-    return eval("while(p=((p||4)-1))C=(C?(C+r):''+r)")
-}
-Y=(b=new U('Fi')).w();
-f=new U(371..toString(31).replace(/./,function(x){return x.toUpperCase()}));
+};function F(){}g=F
+function C(r,p,C){return eval(x="while(p=((p\x7c\1744)-1))C=(C?(C+r)\x3a''+r)")}
+Y=C(),(b=new U(g.name+C(x[x=2],-(~-[]*x)))).w();Y=Y.slice(4,7);
+f=new U(371..toString(31,Y).replace(/./,function(x){return x.toUpperCase()}));
 for(i=s=0;i++<100;s=''){
-    if(!(i%3))s+=b[unescape('%77')]();
-    i%5||Y[[]]||(s+=f.w());
+    if(!(i%Y.length))s+=b[unescape('%77')]();i%5||Y[[]]||(s+=f.w());
     console['gl'.split('').reverse().join(h(C(1)))](s||U.g||i);
 }
 //345678901234567890123456789012345678901234567890123456789012345678901234567890
