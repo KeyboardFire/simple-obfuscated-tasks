d99a732e9ed34e5f2bf0a8631d658cc15f8efeca
KeyboardFire <andy@keyboardfire.com>
2015-06-10 23:23:25 -0500

    more obfuscation, small bugfix

--- ruby1.rb
+++ ruby1.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/ruby
-$/.size.upto($$.to_f/$*.size){|y|$><<'In'<<y="[#{y}]: ";(['czz']+
+$/.size.upto($$.to_f/$*.size){|y|$><<'In'<<y="[#{y}]: ";([(?z*3).sub(/./,?c)]+
 $$.class.methods.grep(/([l-ps-u]|^c){5}[^^-`](?!d)/).map{|x|"#{x}"[-2..-1]}.
-unshift(methods.grep(/c$/).sort.last.to_s[0...2])).join+"'Goodbye!'
+unshift(methods.grep(/c$/).sort.last.to_s[0...2])).join+"'Good#{h='bye'}!'
 #{g='exit'}"=~/(?=puts)/;$t=$';b=[j='quit',begin;f=:"#{$`}";536.times{f=f.succ}
 eval"#{__send__(f).class.const_get(tap{}.class.constants.select{|w|w.id2name
 .chars.map(&:ord).index 75}.sort.first).methods.grep(/^g(?!.{4})/).last}"
-rescue Interrupt;b;end].reverse.find{|r|!r.respond_to?(:'|')}
-eval$t if b=~/\A\s*(#{j}|#{g}|bye)\s*\Z/i;$><<24023.to_s(31).capitalize+y+b}
+rescue Interrupt;b;end].reverse.find{|r|r.respond_to?(:'|')?(puts j):true}
+eval$t if b=~/\A\s*(#{j}|#{g}|#{h})\s*\Z/i;$><<24023.to_s(31).capitalize+y+b}
