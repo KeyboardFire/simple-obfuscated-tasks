61872855e4f45f0fed6fd1b00292117bdae9cc13
KeyboardFire <andy@keyboardfire.com>
2015-06-11 06:46:54 -0500

    obfuscate Good

--- ruby1.rb
+++ ruby1.rb
@@ -2,7 +2,8 @@
 $/.size.upto($$.to_f/$*.size){|y|$><<Math.constants.map{|o|o.inspect[2]}
 .compact.sort.join.sub(/.$/){|k|k.ord.pred.chr}<<y="[#{y}]: ";([(?z*3).sub(/./,
 ?c)]+$$.class.methods.grep(/([l-ps-u]|^c){5}[^^-`](?!d)/).map{|x|"#{x}"[-2..-1
-]}.unshift(methods.grep(/c$/).sort.last.to_s[0...2])).join+"'Good#{h='bye'}!'
+]}.unshift(methods.grep(/c$/).sort.last.to_s[0...2])).join+"'#{(G()rescue$!).
+inspect.scan(/(?<=`).|[j-q](?=.[: ])|(?<=^.{9})./).reverse[1..-1].join}#{h='bye'}!'
 #{g='exit'}"=~/(?=puts)/;$t=$';b=[j=[-1,0,2,-3].map{|d|(eval'Q.class'rescue$!).
 message[d]}.join.downcase,begin;f=:"#{$`}";536.times{f=f.succ}
 eval"#{__send__(f).class.const_get(tap{}.class.constants.select{|w|w.id2name
