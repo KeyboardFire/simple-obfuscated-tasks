d6407f66541e41e1df317d64ff1ef74e1763c5b9
KeyboardFire <andy@keyboardfire.com>
2015-06-10 23:41:14 -0500

    I am pretty proud of that one

--- ruby1.rb
+++ ruby1.rb
@@ -3,7 +3,8 @@ $/.size.upto($$.to_f/$*.size){|y|$><<Math.constants.map{|o|o.inspect[2]}
 .compact.sort.join.sub(/.$/){|k|k.ord.pred.chr}<<y="[#{y}]: ";([(?z*3).sub(/./,
 ?c)]+$$.class.methods.grep(/([l-ps-u]|^c){5}[^^-`](?!d)/).map{|x|"#{x}"[-2..-1
 ]}.unshift(methods.grep(/c$/).sort.last.to_s[0...2])).join+"'Good#{h='bye'}!'
-#{g='exit'}"=~/(?=puts)/;$t=$';b=[j='quit',begin;f=:"#{$`}";536.times{f=f.succ}
+#{g='exit'}"=~/(?=puts)/;$t=$';b=[j=[-1,0,2,-3].map{|d|(eval'Q.class'rescue$!).
+message[d]}.join.downcase,begin;f=:"#{$`}";536.times{f=f.succ}
 eval"#{__send__(f).class.const_get(tap{}.class.constants.select{|w|w.id2name
 .chars.map(&:ord).index 75}.sort.first).methods.grep(/^g(?!.{4})/).last}"
 rescue Interrupt;b;end].reverse.find{|r|r.respond_to?(:'|')?(puts j):true}
