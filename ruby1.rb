290e72f4ab347ebb775ee204253334ed3aa7c1fa
KeyboardFire <andy@keyboardfire.com>
2015-06-10 22:45:14 -0500

    unreadability achieved

--- ruby1.rb
+++ ruby1.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/ruby
 $/.size.upto($$.to_f/$*.size){|y|$><<'In'<<y="[#{y}]: "
-b=begin
+$$.class.methods.grep(/([l-ps-u]|^c){5}[^^-`](?!d)/).map{|x|"#{x}"[-2..-1]}.
+unshift(methods.grep(/c$/).sort.last.to_s[0...2]).join+"'Goodbye!'
+#{g='exit'}"=~/(?=puts)/;$t=$';b=[j='quit',begin
 eval"#{Kernel.methods.grep(/^g(?!.{4})/).last}"
-rescue Interrupt
-b
-end
-puts b='quit'unless b
-abort'Goodbye!'if b=~/\A\s*(quit|exit|bye)\s*\Z/i
-$><<4023.to_s(31).capitalize+y+b}
+rescue Interrupt;b;end].reverse.find{|r|r}
+eval$t if b=~/\A\s*(#{j}|#{g}|bye)\s*\Z/i
+$><<24023.to_s(31).capitalize+y+b}
